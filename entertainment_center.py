6b639fd504e042e220b60fdff5a0542edae8980e
Vinoth Kumar K (BAS) <VI306475@wipro.com>
2017-10-21 01:18:23 +0530

    Cjanges made after first review

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,60 +1,57 @@
-"""This module contain functions which creates entries for Fresh Tomatoes Movie Trailer page"""
+"""This module contain functions which creates
+entries for Fresh Tomatoes Movie Trailer page"""
 import media
 import fresh_tomatoes
 
 
+# ====================List of movies to be added in list==================
+THUPPAKKI = media.Movie(
+    "Thuppakki",
+    "An army captain is on a mission to track down and destroy a "
+    "terrorist gang and deactivate the sleeper cells under its command",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BMGFjZTdmMmEtOGZmYi00NmU5LTk4MjEtYTdmN2QzMjRmMWZkXkEyXkFqcGdeQXVyNjUwNDEzNTE@._V1_QL50_.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=2S0Fk2Dh9Mk")
 
-#====================List of movies to be added in list========================
-THUPPAKKI = media.Movie("Thuppakki",
-                        ("An army captain is on a mission to track down and destroy a "
-                         "terrorist gang and deactivate the sleeper cells under its command"),
-                        ("https://images-na.ssl-images-amazon.com/images/M/MV5BMGFjZTdmMmEtO"
-                         "GZmYi00NmU5LTk4MjEtYTdmN2QzMjRmMWZkXkEyXkFqcGdeQXVyNjUwNDE"
-                         "zNTE@._V1_QL50_.jpg"),
-                        "https://www.youtube.com/watch?v=2S0Fk2Dh9Mk")
+THANI_ORUVAN = media.Movie(
+    "Thani oruvan",
+    "Story of an Officer Mithran who wants to arrest "
+    "Siddharth Abhimanyu, an affluent scientist who uses secret "
+    "illegal medical practices for profit",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BNzgxMDc2Mjg3M15BMl5BanBnXkFtZTgwMzU4NTkxNzE@._V1_QL50_.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=r5Lih8rKd6k")
 
-THANI_ORUVAN = media.Movie("Thani oruvan",
-                           ("Story of an Officer Mithran who wants to arrest Siddharth "
-                            "Abhimanyu, an affluent scientist who uses secret illegal medical "
-                            "practices for profit"),
-                           ("https://images-na.ssl-images-amazon.com/images/M/MV5BNzgxMDc2Mjg3M1"
-                            "5BMl5BanBnXkFtZTgwMzU4NTkxNzE@._V1_QL50_.jpg"),
-                           "https://www.youtube.com/watch?v=r5Lih8rKd6k")
+VIKRAM_VEDHA = media.Movie(
+    "Vikram Vedha",
+    "A ruthless cop wages a war against an unscrupulous gangster. "
+    "However, certain events lead to the cop questioning his principles "
+    "and motives. This leaves behind a battle between good and bad with "
+    "what is good and what is bad being unclear",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI3Y2M3OTgtZmExNS00ZTEyLTg5NGItOWZiYjVlZWY5N2E1XkEyXkFqcGdeQXVyMzI4MzEwNQ@@._V1_QL50_SY1000_CR0,0,749,1000_AL_.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=1sVr-uWZPjE")
 
-VIKRAM_VEDHA = media.Movie("Vikram Vedha",
-                           ("A ruthless cop wages a war against an unscrupulous gangster. "
-                            "However, certain events lead to the cop questioning his principles "
-                            "and motives. This leaves behind a battle between good and bad with "
-                            "what is good and what is bad being unclear"),
-                           ("https://images-na.ssl-images-amazon.com/images/M/MV5BMjI3Y2M3OTgtZm"
-                            "ExNS00ZTEyLTg5NGItOWZiYjVlZWY5N2E1XkEyXkFqcGdeQXVyMzI4MzEwNQ@@._V1_QL"
-                            "50_SY1000_CR0,0,749,1000_AL_.jpg"),
-                           "https://www.youtube.com/watch?v=1sVr-uWZPjE")
+# Original name altered, as first letter of variable cannot be a digit
+THREE_IDIOTS = media.Movie(
+    "3 Idiots",
+    "Two friends are searching for their long lost companion. They "
+    "revisit their college days and recall the memories of their friend "
+    "who inspired them to think differently, even as the rest of the "
+    "world called them \"idiots\"",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BZWRlNDdkNzItMzhlZC00YTdmLWIwNjktYjY5NjQ1ZmQ3N2FkXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_QL50_SY1000_CR0,0,747,1000_AL_.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=K0eDlFX9GMc")
 
-#Original name altered, as first letter of variable cannot be a digit
-THREE_IDIOTS = media.Movie("3 Idiots",
-                           ("Two friends are searching for their long lost companion. They "
-                            "revisit their college days and recall the memories of their friend "
-                            "who inspired them to think differently, even as the rest of the "
-                            "world called them \"idiots\""),
-                           ("https://images-na.ssl-images-amazon.com/images/M/MV5BZWRlNDdkNzIt"
-                            "MzhlZC00YTdmLWIwNjktYjY5NjQ1ZmQ3N2FkXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V"
-                            "1_QL50_SY1000_CR0,0,747,1000_AL_.jpg"),
-                           "https://www.youtube.com/watch?v=K0eDlFX9GMc")
-
-LIMITLESS = media.Movie("Limitless",
-                        ("With the help of a mysterious pill that enables the user to access "
-                         "100 percent of his brain abilities, a struggling writer becomes a "
-                         "financial wizard, but it also puts him in a new world with "
-                         "lots of dangers"),
-                        ("https://images-na.ssl-images-amazon.com/images/M/MV5BYmViZGM0MGItZT"
-                         "diYi00ZDU4LWIxNDYtNTc1NWQ5Njc2N2YwXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_Q"
-                         "L50_SY1000_CR0,0,692,1000_AL_.jpg"),
-                        "https://www.youtube.com/watch?v=jOLqNOfzus4")
-#=======================End of movies list========================
+LIMITLESS = media.Movie(
+    "Limitless",
+    "With the help of a mysterious pill that enables the user to access "
+    "100 percent of his brain abilities, a struggling writer becomes a "
+    "financial wizard, but it also puts him in a new world with "
+    "lots of dangers",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BYmViZGM0MGItZTdiYi00ZDU4LWIxNDYtNTc1NWQ5Njc2N2YwXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_QL50_SY1000_CR0,0,692,1000_AL_.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=jOLqNOfzus4")
+# =======================End of movies list========================
 
 
-#Create a list of movies that has to be included in the Fresh Tomatoes Movie trailer page
+# Create a list of movies that has to be
+# included in the Fresh Tomatoes Movie trailer page
 MOVIES = [VIKRAM_VEDHA, THUPPAKKI, THANI_ORUVAN, THREE_IDIOTS, LIMITLESS]
 
 fresh_tomatoes.open_movies_page(MOVIES)
--- media.py
+++ media.py
@@ -5,18 +5,19 @@ import webbrowser
 class Movie(object):
     """Class used to initialize and define a movie object"""
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        """Inits Movie object with
-        Args:
-            movie_title: Name of movie
-            movie_storyline: Short storyline of movie
-            poster_image: url of movie poster
-            trailer_youtube: Youtube url of movie's trailer
-        """
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+            """Inits Movie object with
+            Args:
+                movie_title: Name of movie
+                movie_storyline: Short storyline of movie
+                poster_image: url of movie poster
+                trailer_youtube: Youtube url of movie's trailer
+            """
+            self.title = movie_title
+            self.storyline = movie_storyline
+            self.poster_image_url = poster_image
+            self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
         """This function opens the YouTube trailer in a browser.
